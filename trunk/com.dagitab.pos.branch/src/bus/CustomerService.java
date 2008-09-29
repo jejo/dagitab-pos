8fd4b08dc4f320aa180c8d386e059b362a7cb36c
jejmaster <jejmaster@b133cdf1-f851-0410-a052-9d1110f0934b>
2008-09-29 06:42:56 +0000

    Changed the way of fetching records by preferred sort order

--- trunk/com.dagitab.pos.branch/src/bus/CustomerService.java
+++ trunk/com.dagitab.pos.branch/src/bus/CustomerService.java
@@ -6,7 +6,7 @@ import main.Main;
 
 public class CustomerService {
 	public static ResultSet getAllCustomers(){
-		ResultSet rs = Main.getDBManager().executeQuery("SELECT * FROM customer_lu");
+		ResultSet rs = Main.getDBManager().executeQuery("SELECT * FROM customer_lu ORDER by LAST_NAME");
 		return rs;
 	}
 	
@@ -14,7 +14,7 @@ public class CustomerService {
 		ResultSet rs = Main.getDBManager().executeQuery("SELECT * FROM customer_lu " +
 				"WHERE CUST_NO LIKE '%"+s+"%' " +
 				"OR LAST_NAME LIKE '%"+s+"%' " +
-				"OR FIRST_NAME LIKE '%"+s+"%'");
+				"OR FIRST_NAME LIKE '%"+s+"%' ORDER by LAST_NAME");
 		return rs;
 	}
 }
