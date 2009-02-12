d5065859f31e59fa9c59dfd7c67bad53851541da
jejmaster <jejmaster@b133cdf1-f851-0410-a052-9d1110f0934b>
2009-02-12 07:21:17 +0000

    Fixed Eastwood Sales Report

--- trunk/com.dagitab.pos.branch/src/com/dagitab/pos/reports/EastwoodSalesReport.java
+++ trunk/com.dagitab.pos.branch/src/com/dagitab/pos/reports/EastwoodSalesReport.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFCell;
@@ -31,6 +32,7 @@ public class EastwoodSalesReport {
 	private Double totalCashSales = 0.0d;
 	private Double totalCreditSales = 0.0d;
 	private Double totalGCSales = 0.0d;
+	private Double totalNetSales = 0.0d;
 	
 	public boolean generate(String fileName, int month, int year) throws Exception{
 		
@@ -44,8 +46,9 @@ public class EastwoodSalesReport {
 			HSSFSheet sheet = wb.getSheetAt(0);
 			
 			ReportUtility.writeDateGenerated(wb);
-			
+			logger.debug("JEJO MONTH PASSED: "+month+" "+year);
 			ArrayList<String> dates = ReportService.getInstance().getEastwoodInvoiceDates(month, year);
+			Calendar calendar = new GregorianCalendar();
 			
 			for(int i = 0; i<dates.size(); i++){
 				
@@ -53,33 +56,37 @@ public class EastwoodSalesReport {
 				System.out.println("Dates: "+dates.get(i));
 				cell = HSSFUtil.createStringCell(wb, row, (short) 0,false,true);
 				cell.setCellValue(dates.get(i));
-				Calendar calendar = Calendar.getInstance();
 				
-				calendar.set(Calendar.MONTH, Integer.parseInt(dates.get(i).split("-")[1]));
+				calendar.set(Calendar.MONTH, Integer.parseInt(dates.get(i).split("-")[1])-1);
+				logger.debug(dates.get(i).split("-")[1]);
 				calendar.set(Calendar.YEAR, Integer.parseInt(dates.get(i).split("-")[0]));
+				logger.debug(dates.get(i).split("-")[0]);
 				calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dates.get(i).split("-")[2]));
+				logger.debug(dates.get(i).split("-")[2]);
+				
 				
 				calendar.set(Calendar.HOUR_OF_DAY, 9); // set to 9am
 				calendar.set(Calendar.MINUTE, 0);
 				calendar.set(Calendar.SECOND, 0);
 				
+				logger.debug("JEJO DATE GENERATED: "+calendar.getTime().toString());
 				
-				
-				Double cashSales = ComplianceService.getComplianceService().getSalesForPaymentTypeWithoutDiscount(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()), 1);
+				Double cashSales = ComplianceService.getComplianceService().getSalesForPaymentTypeWithoutDiscount(calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()), 1);
+				logger.debug("JEJO CASH SALES AT: "+(calendar.get(Calendar.MONTH)+1)+" "+calendar.get(Calendar.DAY_OF_MONTH)+": "+cashSales);
 				cell = HSSFUtil.createAmountCell(wb, row, (short) 1, false, true);
 				cell.setCellValue(cashSales);
 				
 				
-				Double creditSales = ComplianceService.getComplianceService().getSalesForPaymentTypeWithoutDiscount(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()), 3);
+				Double creditSales = ComplianceService.getComplianceService().getSalesForPaymentTypeWithoutDiscount(calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()), 3);
 				cell = HSSFUtil.createAmountCell(wb, row, (short) 2, false, true);
 				cell.setCellValue(creditSales);
 				
-				Double gcSales = ComplianceService.getComplianceService().getSalesForPaymentTypeWithoutDiscount(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()), 4);
+				Double gcSales = ComplianceService.getComplianceService().getSalesForPaymentTypeWithoutDiscount(calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()), 4);
 				cell = HSSFUtil.createAmountCell(wb, row, (short) 3, false, true);
 				cell.setCellValue(gcSales);
 				
-				Double grossSalesAmount = ComplianceService.getComplianceService().getRawGross(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()));
-				Double netSalesAmount = ComplianceService.getComplianceService().getNetSales(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()));
+				Double grossSalesAmount = ComplianceService.getComplianceService().getRawGross(calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()));
+				Double netSalesAmount = ComplianceService.getComplianceService().getNetSales(calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()));
 				Double totalDiscount = grossSalesAmount - netSalesAmount;
 				
 				cell = HSSFUtil.createAmountCell(wb, row, (short) 4, false, true);
@@ -91,10 +98,16 @@ public class EastwoodSalesReport {
 				cell = HSSFUtil.createAmountCell(wb, row, (short) 6, false, true);
 				cell.setCellValue(0.0d);
 				
-//				Double grossSales = ReportService.getInstance().getGrossSales(dates.get(i));
-				Double grossSales = ReportService.getInstance().getNetSalesBeforeTax(dates.get(i));
+				Double grossSales = ReportService.getInstance().getGrossSales(dates.get(i));
+//				Double grossSales = ReportService.getInstance().getNetSalesBeforeTax(dates.get(i));
 				cell = HSSFUtil.createAmountCell(wb, row, (short) 7, false, true);
 				cell.setCellValue(grossSales);
+				
+				
+				Double netSales = ComplianceService.getComplianceService().getNetSales(calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),  Integer.parseInt(Main.getStoreCode()));
+//				Double grossSales = ReportService.getInstance().getNetSalesBeforeTax(dates.get(i));
+				cell = HSSFUtil.createAmountCell(wb, row, (short) 8, false, true);
+				cell.setCellValue(netSales);
 				
 				
 				totalGrossSales += grossSales;
@@ -103,6 +116,7 @@ public class EastwoodSalesReport {
 				totalCashSales += cashSales;
 				totalGCSales += gcSales;
 				totalCreditSales += creditSales;
+				totalNetSales += netSales;
 				
 				rowCounter++;
 				
@@ -128,7 +142,8 @@ public class EastwoodSalesReport {
 			cell.setCellValue(0.0d);
 			cell = HSSFUtil.createAmountCell(wb, row, (short) 7,false,true);
 			cell.setCellValue(totalGrossSales);
-			
+			cell = HSSFUtil.createAmountCell(wb, row, (short) 8,false,true);
+			cell.setCellValue(totalNetSales);
 			rowCounter++;
 			
 			ReportUtility.writeOutputToFile(wb, fileName);
